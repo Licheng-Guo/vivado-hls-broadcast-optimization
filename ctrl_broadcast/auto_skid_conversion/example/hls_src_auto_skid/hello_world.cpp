3a1720beffcca50ec39d125a5f0c458ef1bd68d7
Licheng-Guo <lichengguo777@gmail.com>
2020-06-19 11:02:42 -0700

    fix indent

--- ctrl_broadcast/auto_skid_conversion/example/hls_src_auto_skid/hello_world.cpp
+++ ctrl_broadcast/auto_skid_conversion/example/hls_src_auto_skid/hello_world.cpp
@@ -5,41 +5,41 @@ using data_t = int;
 
 
 void module_add(
-	data_t array_in[SIZE],
-	hls::stream<data_t>& fifo1,
-	hls::stream<data_t>& fifo2
+  data_t array_in[SIZE],
+  hls::stream<data_t>& fifo1,
+  hls::stream<data_t>& fifo2
 ) {
 #pragma HLS inline off
-	for (int i = 0; i < SIZE;i++) {
+  for (int i = 0; i < SIZE;i++) {
 #pragma HLS pipeline II=4
-		fifo1.write_nb(array_in[i]+i);
-		fifo2.write_nb(array_in[i]+i);
-	}
+    fifo1.write_nb(array_in[i]+i);
+    fifo2.write_nb(array_in[i]+i);
+  }
 
 }
 
 void module_sub(
-	hls::stream<data_t>& fifo1,
-	hls::stream<data_t>& fifo2,
-	data_t array_out[SIZE]
+  hls::stream<data_t>& fifo1,
+  hls::stream<data_t>& fifo2,
+  data_t array_out[SIZE]
 ) {
 #pragma HLS inline off
-	for (int i = 0; i < SIZE; i++) {
+  for (int i = 0; i < SIZE; i++) {
 #pragma HLS pipeline II=1
-		data_t temp1, temp2;
+    data_t temp1, temp2;
     if (!fifo1.empty() & !fifo2.empty()){
-			fifo1.read_nb(temp1);
-			fifo2.read_nb(temp2);
-			array_out[i] = (temp1*temp2*i);
+      fifo1.read_nb(temp1);
+      fifo2.read_nb(temp2);
+      array_out[i] = (temp1*temp2*i);
     }
-	}
+  }
 
 }
 
 extern "C" {
 void top(
-	data_t array_in[SIZE],
-	data_t array_out[SIZE]
+  data_t array_in[SIZE],
+  data_t array_out[SIZE]
 ) {
   int size = SIZE;
 #pragma HLS INTERFACE m_axi port=array_in offset=slave bundle=gmem0 depth=size
@@ -47,12 +47,12 @@ void top(
 #pragma HLS INTERFACE s_axilite port=return bundle=control
 
 #pragma HLS dataflow
-	hls::stream<data_t> fifo1;
+  hls::stream<data_t> fifo1;
 #pragma HLS stream variable=fifo1 depth=4
-	hls::stream<data_t> fifo2;
+  hls::stream<data_t> fifo2;
 #pragma HLS stream variable=fifo2 depth=4
 
-	module_add(array_in, fifo1, fifo2);
-	module_sub(fifo1, fifo2, array_out);
+  module_add(array_in, fifo1, fifo2);
+  module_sub(fifo1, fifo2, array_out);
 }
 }
--- ctrl_broadcast/auto_skid_conversion/example/hls_tb/test_hello_world.cpp
+++ ctrl_broadcast/auto_skid_conversion/example/hls_tb/test_hello_world.cpp
@@ -5,20 +5,20 @@ void top(data_t array_in[SIZE], data_t array_out[SIZE]);
 
 int main()
 {
-	data_t array[SIZE];
-	data_t array_out[SIZE];
-	for (int i = 0; i < SIZE; i++) {
-		array[i] = i;
-		array_out[i] = 0;
-	}
+  data_t array[SIZE];
+  data_t array_out[SIZE];
+  for (int i = 0; i < SIZE; i++) {
+    array[i] = i;
+    array_out[i] = 0;
+  }
 
-	top(array, array_out);
+  top(array, array_out);
 
-	for (int i = 0; i < SIZE; i++) {
-		if ((array[i]+i)*(array[i]+i)*i != array_out[i]) {
-			return -1;
-		}
-	}
+  for (int i = 0; i < SIZE; i++) {
+    if ((array[i]+i)*(array[i]+i)*i != array_out[i]) {
+      return -1;
+    }
+  }
 
-	return 0;
+  return 0;
 }
